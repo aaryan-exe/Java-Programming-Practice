98193ca6fffa22454e3bc370a8274569adfe3c96
aryan89288 <aryan89288@gmail.com>
2024-01-13 20:00:47 +0530

    Create real_number.java

---
+++ Practice Programs/real_number.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class real_number {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        int i, sum = 0;
+
+        for (i = 1; i < n; i++) {
+            if (n % i == 0)
+             {
+                sum = sum + i;
+            }
+        }
+        if (sum == n)
+       {
+            System.out.println("It is real number");
+        } 
+        else
+        {
+            System.out.println("It is not a real number");
+        }
+
+    }
+}
