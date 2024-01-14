e9ba2e7cae547de1d06546b3eb28880c3c9c317b
aryan89288 <aryan89288@gmail.com>
2024-01-14 13:05:11 +0530

    Update

--- Practice Programs/real_number.java
+++ Practice Programs/real_number.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class real_number {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter number");
@@ -10,17 +9,13 @@ public class real_number {
         int i, sum = 0;
 
         for (i = 1; i < n; i++) {
-            if (n % i == 0)
-             {
+            if (n % i == 0) {
                 sum = sum + i;
             }
         }
-        if (sum == n)
-       {
+        if (sum == n) {
             System.out.println("It is real number");
-        } 
-        else
-        {
+        } else {
             System.out.println("It is not a real number");
         }
 
---
+++ Practice Programs/rock_paper_scissor_game.java
@@ -0,0 +1,84 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class rock_paper_scissor_game {
+    public static void main(String[] args) {
+        int i;
+        Scanner sc = new Scanner(System.in);
+        Random ran = new Random();
+        int a = ran.nextInt(1, 4);
+            System.out.println("Enter \n 1 for rock \n 2 for paper \n 3 for scissor");
+            int n = sc.nextInt();
+
+            // User command
+            if (n == 1) {
+                System.out.println("You have choosen rock");
+            }
+            if (n == 2) {
+                System.out.println("You have choosen paper");
+            }
+            if (n == 3) {
+                System.out.println("You have choosen scissor");
+            }
+
+            if (n > 3) {
+                System.out.println("Wrong inpput");
+            }
+
+            // Pc command
+            if (a == 1) {
+                System.out.println("System have choosen rock");
+            }
+            if (a == 2) {
+                System.out.println("System have choosen paper");
+            }
+            if (a == 3) {
+                System.out.println("System have choosen scissor");
+            }
+
+            // Win/Loose message (Optional)
+
+            // Rock
+            if (a == 1 && n == 3) {
+                System.out.println("You loose!");
+            }
+
+            if (a == 1 && n == 2) {
+                System.out.println("You win!");
+            }
+
+            if (a == 1 && n == 1) {
+                System.out.println("Tie!");
+            }
+
+            // paper
+
+            if (a == 2 && n == 3) {
+                System.out.println("You win!");
+            }
+
+            if (a == 2 && n == 1) {
+                System.out.println("You loose!");
+            }
+
+            if (a == 2 && n == 2) {
+                System.out.println("Tie!");
+            }
+
+            // paper
+
+            if (a == 3 && n == 2) {
+                System.out.println("You loose!");
+            }
+
+            if (a == 3 && n == 1) {
+                System.out.println("You win!");
+            }
+
+            if (a == 3 && n == 3) {
+                System.out.println("Tie!");
+            }
+
+        
+    }
+}
