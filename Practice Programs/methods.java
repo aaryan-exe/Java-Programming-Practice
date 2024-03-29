030b5614a64fe8da27a3c32f7bad567486b9055d
aryan89288 <aryan89288@gmail.com>
2024-03-29 13:33:40 +0530

    Create methods.java

---
+++ Practice Programs/methods.java
@@ -0,0 +1,17 @@
+//table method
+public class methods {
+    // method
+    int multi(int a) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(a * i);
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        methods Obj = new methods();
+        Obj.multi(n);
+
+    }
+}
